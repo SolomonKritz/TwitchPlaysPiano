2652118b6010ee8e73e968ed2b90c039a0d34f22
SolomonKritz <solomonkritz@gmail.com>
2016-04-09 01:58:27 -0400

    SK - Copied a function over
    import wasn't working, other files probably need to be in a directory?

--- convert.py
+++ convert.py
@@ -1,29 +1,23 @@
-
-
-
 def convert(string):
     parts = string.split(",")
     octave = 0
     if len(parts) >= 2:
-	octave = int(parts[1])
+        octave = int(parts[1])
     sharp = (1 if parts[0].isupper() else 0)
     key = 0
     key_lower = parts[0].lower()
     if key_lower == 'a':
-	key = -3
+        key = -3
     elif key_lower == 'b':
-	key = -1
+        key = -1
     elif key_lower == 'c':
-	key = 0
+        key = 0
     elif key_lower == 'd':
-	key = 2
+        key = 2
     elif key_lower == 'e':
-	key = 4
+        key = 4
     elif key_lower == 'f':
-	key = 5
+        key = 5
     elif key_lower == 'g':
-	key = 7
-
+        key = 7
     return 72 + key + 12 * octave + sharp
-
-print(convert('c'))
--- twitch_irc.py
+++ twitch_irc.py
@@ -52,8 +52,34 @@ def connect_to_channel(channel_name):
               note = note.replace(",-", "")
               break
         if (note is not ""):
+           num = convert(note)
            print(note)
+           print(num)
         time.sleep(beatLen)
+
+def convert(string):
+    parts = string.split(",")
+    octave = 0
+    if len(parts) >= 2:
+        octave = int(parts[1])
+    sharp = (1 if parts[0].isupper() else 0)
+    key = 0
+    key_lower = parts[0].lower()
+    if key_lower == 'a':
+        key = -3
+    elif key_lower == 'b':
+        key = -1
+    elif key_lower == 'c':
+        key = 0
+    elif key_lower == 'd':
+        key = 2
+    elif key_lower == 'e':
+        key = 4
+    elif key_lower == 'f':
+        key = 5
+    elif key_lower == 'g':
+        key = 7
+    return 72 + key + 12 * octave + sharp
 
 if __name__ == "__main__":
     channel_name = input("Enter a channel name: ")
